a61ebd3f7310b1d6bb68d6026d3fee766e3cd384
ohm <ohm.pongsakorn2@gmail.com>
2021-04-13 13:39:01 +0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
+  res.send({ byeee: 'buddyeee' });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
