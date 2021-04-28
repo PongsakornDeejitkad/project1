4a31fa5619896b0ef245721388535ada614e7c2e
pongsakornDeejitkad <ohm.pongsakorn2@gmail.com>
2021-04-28 23:54:17 +0700

    Update28042021

--- backeng dev/index.js
+++ backeng dev/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
-require('.routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
