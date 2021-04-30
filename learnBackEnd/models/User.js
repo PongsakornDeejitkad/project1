7a3ebae2a73d681a768968d7e42804e97c315925
pongsakornDeejitkad <ohm.pongsakorn2@gmail.com>
2021-04-30 17:51:32 +0700

    Update30042021

--- learnBackEnd/models/User.js
+++ learnBackEnd/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const userSchema = new Schema({
-  googleId: String,
+  googleId: Number,
 });
 
 mongoose.model('users', userSchema);
--- learnBackEnd/services/passport.js
+++ learnBackEnd/services/passport.js
@@ -13,7 +13,12 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existinguser => {
+        if (existinguser) {
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
