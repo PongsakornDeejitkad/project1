9be1a5c8f3620ba226988d2f652aafffa0277c5f
pongsakornDeejitkad <ohm.pongsakorn2@gmail.com>
2021-05-04 17:57:44 +0700

    Update04052021

--- learnBackEnd/services/passport.js
+++ learnBackEnd/services/passport.js
@@ -4,6 +4,15 @@ const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
+passport.serializeUser((user, done) => {
+  done(null, user.id); //id from mongodb
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -13,10 +22,13 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existinguser => {
-        if (existinguser) {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
         } else {
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
