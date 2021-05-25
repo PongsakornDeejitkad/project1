bae83cf08f710b9870e70fd721c65c2310a64d5f
pongsakornDeejitkad <ohm.pongsakorn2@gmail.com>
2021-05-25 16:41:39 +0700

    update login google

---
+++ learnBackEnd/test.js
@@ -0,0 +1,31 @@
+// write a function to retrieve a blob json
+// make an ajax requset use the 'fetch' function
+// https://reqres.in/api/products/3
+
+// first
+// function fetchAlbums() {
+//   fetch('https://reqres.in/api/products/3')
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+
+// fetchAlbums();
+
+//Secound
+
+// async function fetchAlbums() {
+//   const res = await fetch('https://reqres.in/api/products/3');
+//   const json = await res.json();
+
+//   console.log(json);
+// }
+// fetchAlbums();
+
+//Third
+// const fetchAlbums = async () => {
+//   const res = await fetch('https://reqres.in/api/products/3');
+//   const json = await res.json();
+
+//   console.log(json);
+// };
+// fetchAlbums();
